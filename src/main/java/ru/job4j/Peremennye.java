90b9616b957da4463e60379202a77fc230ba434c
EgorStarosvetskiy <starosvetsky@mail.ru>
2022-01-25 19:15:55 +0300

    Peremennya

--- src/main/java/ru/job4j/Peremennye.java
+++ src/main/java/ru/job4j/Peremennye.java
@@ -4,7 +4,6 @@ public class Peremennye {
     public static void main(String[] args) {
         int size = 10;
         size = size + 90;
-        System.out.println(size);
         size = size - 5;
         System.out.println(size);
     }
