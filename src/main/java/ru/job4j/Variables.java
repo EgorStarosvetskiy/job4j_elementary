e34bd41f8ea82d2c5afaa096fadb29ddb755eec7
EgorStarosvetskiy <starosvetsky@mail.ru>
2022-01-27 18:53:08 +0300

    Peremennya

---
+++ src/main/java/ru/job4j/Variables.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        int size = 10;
+        size = 100;
+        size = 100 - 5;
+        System.out.println(size);
+    }
+}
